bc56ce87a9aa01cdae5cc5c52a76f04ede3ebf1a
djmisieq <zadroga.d@gmail.com>
2025-03-14 19:29:45 +0100

    Update Next.js config to handle circular dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Fix for circular dependencies
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      fs: false,
+      module: false,
+    };
+    
+    // Turn off webpack's circular dependency warning
+    config.ignoreWarnings = [/circular dependency/];
+    
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
