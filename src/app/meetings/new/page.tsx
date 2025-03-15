afe29269ae617ed0959906000329c2084038d8e2
djmisieq <zadroga.d@gmail.com>
2025-03-15 21:06:21 +0100

    Add new meeting page

---
+++ src/app/meetings/new/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import MeetingForm from '@/components/meetings/meeting-form';
+
+export default function NewMeetingPage() {
+  return <MeetingForm />;
+}
